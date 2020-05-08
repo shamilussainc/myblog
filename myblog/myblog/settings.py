d87a12adaf8c55b99000854981aaa005f3be4e8b
shamilussainc <shamilhussainc@gmail.com>
2020-05-08 14:00:26 +0530

    added_sharebymail

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -119,3 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'putthemail@gmail.com'
+EMAIL_HOST_PASSWORD = 'mailpassword'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
+
